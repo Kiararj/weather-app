692c5adcc54be1447044ffa616a9aa38542825ee
Kiara Johnson <kiarajohnson@kiaras-mbp.lan>
2023-07-24 07:41:01 -0500

    added some functions

--- assets/script.js
+++ assets/script.js
@@ -1,2 +1,33 @@
 const apiKey = '5fd4a8f8e99d3f5261874c1f8fe806e3';
 
+var cityText = document.getElementById('city-input');
+var searchButton = document.getElementById('search-btn');
+
+function getCoordinate(coordinateApi) {
+    fetch(coordinateApi)
+    .then((response) => {
+        return response.json();
+    })
+    .then((response) => {
+        userWeather(response[0].lat,response[0].lon)
+
+    })
+}
+
+function userWeather(lat,lon){
+    var weatherApi = 'https://api.openweathermap.org/data/2.5/forecast?lat=' + lat + '&lon' + lon + '&appid=' + apiKey + '&units=imperial'
+    fetch(weatherApi)
+    .then((response) => {
+
+return response.json();
+    })
+.then((response) => {
+console.log(response);
+});
+}
+
+searchButton.addEventListener('click', function () {
+    var textInput = cityText.value
+    var coordinateApi = 'https://api.openweathermap.org/geo/1.0/direct?q=' + textInput 
+    getCoordinate(coordinateApi)
+});
